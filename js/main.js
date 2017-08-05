bd00028dd75cb5c879066431cd1111e9a60ac3c1
Michael Chadwick <mike@codana.me>
2017-08-05 13:44:26 -0700

    minor refactoring

--- js/main.js
+++ js/main.js
@@ -1,37 +1,45 @@
-$(function() {
-  function initSites() {
-    $toc = $("select#toc");
-    $sites = $("article#sites");
-    $.getJSON("json/sites.json")
-    .success(function(data) {
-      $sites.html("");
-      $.each(data, function(k, v) {
-        $toc.append("<option name='" + v.name.replace(/\s/g, '').toLowerCase() + "'>" + v.name + "</option>");
-        $sites.append("<a name='" + v.name.replace(/\s/g, '').toLowerCase() + "'></a><section><h2>" + v.name + "</h2><h3><a href='" + v.url + "'>" + v.url + "</a></h3><div class='iframe-frame'><iframe data-src='" + v.url + "' height='200'></iframe></div></section>");
-      });
+/* global $ */
+$(function ($) {
+  function initSites () {
+    var $toc = $('select#toc')
+    var $sites = $('article#sites')
+    $.getJSON('json/sites.json')
+    .success(function (data) {
+      $sites.html('')
+      // <article>
+      $.each(data, function (k, v) {
+        $toc.append(
+          `<option name='${v.name.replace(/\s/g, '').toLowerCase()}'>${v.name}</option>`
+        )
+        var html = ''
+        html += `<a name='${v.name.replace(/\s/g, '').toLowerCase()}'></a>`
+        html += `<section id='site-${v.name.replace(/\s/g, '').toLowerCase()}'>`
+        html += `  <h2 class='site-header'>${v.name}</h2>`
+        html += `  <h3 class='site-link'><a href='${v.url}'>${v.url}</a></h3>`
+        html += `  <div class='iframe-frame'>`
+        html += `    <iframe id='iframe-${v.name.replace(/\s/g, '').toLowerCase()}' data-src='${v.url}' height='200'></iframe>`
+        html += `  </div>`
+        html += `</section>`
+        $sites.append(html)
+      })
+      // </article>
     })
-    .error(function(e) {
-      if (e.status == 404)
-        $sites.html("<p>Error: json/sites.json is missing</p>");
-      else {
-        $sites.html("<p>Error: json/sites.json is malformed</p>");
-        $sites.append("<textarea id='json' style='border: 1px solid #000; font-family: Consolas; height: 200px; width: 500px;'>" + e.responseText + "</textarea>");
+    .error(function (e) {
+      if (e.status === 404) {
+        $sites.html('<p>Error: json/sites.json is missing</p>')
+      } else {
+        $sites.html('<p>Error: json/sites.json is malformed</p>')
+        $sites.append(`<textarea id='json' style='border: 1px solid #000; font-family: Consolas; height: 200px; width: 500px;'>${e.responseText}</textarea>`)
       }
-    });
-  };
+    })
+  }
 
-  $("select#toc").change(function() {
-    var anchor = $("select#toc option:selected").attr("name");
-    newurl = document.location.protocol + "//" + document.location.hostname + ":" + document.location.port + "/" + "#" + anchor;
-    window.location.href = newurl;
-  });
-
-  $("section iframe").recliner({
-    attrib: "data-src", // selector for attribute containing the media src
+  $('section iframe').recliner({
+    attrib: 'data-src', // selector for attribute containing the media src
     throttle: 300,      // millisecond interval at which to process events
     threshold: 100,     // scroll distance from element before its loaded
     live: true          // auto bind lazy loading to ajax loaded elements
-  });
+  })
 
-  initSites();
-});
+  initSites()
+})
