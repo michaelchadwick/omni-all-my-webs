f7b99fa1ccb8961c2e8064c683ec8ba188e6c6b0
Michael Chadwick <michael.chadwick@gmail.com>
2018-03-05 22:53:47 -0800

    https sites now have green border

--- js/main.js
+++ js/main.js
@@ -2,44 +2,51 @@
 $(function ($) {
   var $toc = $('select#toc')
   var $sites = $('article#sites')
+  var extraClass = ''
 
   function initSites () {
     $.getJSON('json/sites.json')
-    .success(function (data) {
-      $sites.html('')
-      // <article>
-      $.each(data, function (k, v) {
-        $toc.append(
-          `<option name='${v.name.replace(/\s/g, '').toLowerCase()}'>${v.name}</option>`
-        )
-        var html = ''
-        html += `<a name='${v.name.replace(/\s/g, '').toLowerCase()}'></a>`
-        html += `<section id='site-${v.name.replace(/\s/g, '').toLowerCase()}'>`
-        html += `  <h2 class='site-header'>${v.name}</h2>`
-        html += `  <h3 class='site-link'><a href='${v.url}' target='_blank'>${v.url}</a></h3>`
-        html += `  <div class='iframe-frame'>`
-        html += `    <iframe id='iframe-${v.name.replace(/\s/g, '').toLowerCase()}' data-src='${v.url}' height='200'></iframe>`
-        html += `  </div>`
-        html += `</section>`
-        $sites.append(html)
-      })
+      .success(function (data) {
+        $sites.html('')
+        // <article>
+        $.each(data, function (k, v) {
+          if (v.url.indexOf('https') >= 0) {
+            extraClass = 'ssl'
+            console.log(`${v.url} has https`)
+          } else {
+            extraClass = ''
+          }
+          $toc.append(
+            `<option name='${v.name.replace(/\s/g, '').toLowerCase()}'>${v.name}</option>`
+          )
+          var html = ''
+          html += `<a name='${v.name.replace(/\s/g, '').toLowerCase()}'></a>`
+          html += `<section id='site-${v.name.replace(/\s/g, '').toLowerCase()}'>`
+          html += `  <h2 class='site-header ${extraClass}'>${v.name}</h2>`
+          html += `  <h3 class='site-link ${extraClass}'><a href='${v.url}' target='_blank'>${v.url}</a></h3>`
+          html += `  <div class='iframe-frame ${extraClass}'>`
+          html += `    <iframe id='iframe-${v.name.replace(/\s/g, '').toLowerCase()}' data-src='${v.url}' height='200'></iframe>`
+          html += `  </div>`
+          html += `</section>`
+          $sites.append(html)
+        })
       // </article>
-    })
-    .error(function (e) {
-      if (e.status === 404) {
-        $sites.html('<p>Error: json/sites.json is missing</p>')
-      } else {
-        $sites.html('<p>Error: json/sites.json is malformed</p>')
-        $sites.append(`<textarea id='json' style='border: 1px solid #000; font-family: Consolas; height: 200px; width: 500px;'>${e.responseText}</textarea>`)
-      }
-    })
+      })
+      .error(function (e) {
+        if (e.status === 404) {
+          $sites.html('<p>Error: json/sites.json is missing</p>')
+        } else {
+          $sites.html('<p>Error: json/sites.json is malformed</p>')
+          $sites.append(`<textarea id='json' style='border: 1px solid #000; font-family: Consolas; height: 200px; width: 500px;'>${e.responseText}</textarea>`)
+        }
+      })
   }
 
   $('section iframe').recliner({
     attrib: 'data-src', // selector for attribute containing the media src
-    throttle: 300,      // millisecond interval at which to process events
-    threshold: 100,     // scroll distance from element before its loaded
-    live: true          // auto bind lazy loading to ajax loaded elements
+    throttle: 300, // millisecond interval at which to process events
+    threshold: 100, // scroll distance from element before its loaded
+    live: true // auto bind lazy loading to ajax loaded elements
   })
 
   $toc.on('change', function () {
