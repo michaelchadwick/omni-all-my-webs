2bb31db9720fb0d12d97210b0dd2bacb9f75bb0f
Michael Chadwick <michael.chadwick@gmail.com>
2023-05-31 13:38:12 -0700

    fixed jquery $.getJSON call

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 (function ($) {
-  function create_site(v, $article, cl = '') {
+  function create_site(site, $article, cl = '') {
     // create anchor
     var anchor = document.createElement('a')
-    anchor.setAttribute('name', v.name.replace(/\s/g, '').toLowerCase())
+    anchor.setAttribute('name', site.name.replace(/\s/g, '').toLowerCase())
     $article.append(anchor)
 
     // create section
     var section = document.createElement('section')
-    section.id = `site-${v.name.replace(/\s/g, '').toLowerCase()}`
+    section.id = `site-${site.name.replace(/\s/g, '').toLowerCase()}`
     if (cl !== '') section.classList.add(cl)
 
       // create inner
@@ -18,26 +18,26 @@
         var site_header = document.createElement('h2')
         site_header.classList.add('site-header')
         if (cl !== '') site_header.classList.add(cl)
-        site_header.innerHTML = v.name
+        site_header.innerHTML = site.name
 
         // e.g. Site is for awesomeness
         var site_blurb = document.createElement('h3')
         site_blurb.classList.add('site-blurb')
         if (cl !== '') site_blurb.classList.add(cl)
-        site_blurb.innerHTML = v.blurb
+        site_blurb.innerHTML = site.blurb
 
         // e.g. HTML, CSS, 6502 Assembly
         var site_tech = document.createElement('div')
         site_tech.classList.add('site-tech')
         if (cl !== '') site_tech.classList.add(cl)
-        site_tech.innerHTML = v.tech
+        site_tech.innerHTML = site.tech
 
         // e.g. https://cool.site
         var site_link = document.createElement('h5')
         site_link.classList.add('site-link')
         var site_link_anchor = document.createElement('a')
-        site_link_anchor.href = v.url
-        site_link_anchor.innerHTML = v.url
+        site_link_anchor.href = site.url
+        site_link_anchor.innerHTML = site.url
         site_link_anchor.target = '_blank'
 
         site_link.append(site_link_anchor)
@@ -46,8 +46,8 @@
         var site_iframe_frame = document.createElement('div')
         site_iframe_frame.classList.add('iframe-frame')
         var site_iframe = document.createElement('iframe')
-        site_iframe.id = `iframe-${v.name.replace(/\s/g, '').toLowerCase()}`
-        site_iframe.setAttribute('src', v.url)
+        site_iframe.id = `iframe-${site.name.replace(/\s/g, '').toLowerCase()}`
+        site_iframe.setAttribute('src', site.url)
         site_iframe.height = '300'
         site_iframe.addEventListener(
           'load',
@@ -75,45 +75,44 @@
   }
 
   // pull site creation data
-  $.getJSON('assets/json/sites.json')
-    .success(function (data) {
-      var $current = $('article#sites-current')
-      var $archived = $('article#sites-archived')
+  $.getJSON('assets/json/sites.json', (data) => {
+    var $current = $('article#sites-current')
+    var $archived = $('article#sites-archived')
 
-      $current.html('')
-      $archived.html('')
+    $current.html('')
+    $archived.html('')
 
-      var article_height = 0
-      // <article id='sites-current'>
-      $.each(data.current, (k, v) => {
-        create_site(v, $current)
-        article_height += 350
-      })
-      // </article>
-
-      // <article id='sites-archived'>
-      $.each(data.archived, (k, v) => {
-        create_site(v, $archived, 'archived')
-      })
-      // </article>
-
-      // figure out height of <article>
-      var height_mod = 0.615
-
-      if (window.innerWidth >= 1030) {
-        $current.height(article_height * height_mod)
-      } else {
-        $current.height('auto')
-      }
+    var article_height = 0
+    // <article id='sites-current'>
+    $.each(data.current, (k, v) => {
+      create_site(v, $current)
+      article_height += 350
     })
-    .error(function (e) {
-      if (e.status === 404) {
-        $sites.html('<p>Error: json/sites.json is missing</p>')
-      } else {
-        $sites.html('<p>Error: json/sites.json is malformed</p>')
-        $sites.append(`<textarea id='json' style='border: 1px solid #000; font-family: Consolas; height: 200px; width: 500px;'>${e.responseText}</textarea>`)
-      }
+    // </article>
+
+    // <article id='sites-archived'>
+    $.each(data.archived, (k, v) => {
+      create_site(v, $archived, 'archived')
     })
+    // </article>
+
+    // figure out height of <article>
+    var height_mod = 0.615
+
+    if (window.innerWidth >= 1030) {
+      $current.height(article_height * height_mod)
+    } else {
+      $current.height('auto')
+    }
+  })
+  .fail(function (e) {
+    if (e.status === 404) {
+      $sites.html('<p>Error: json/sites.json is missing</p>')
+    } else {
+      $sites.html('<p>Error: json/sites.json is malformed</p>')
+      $sites.append(`<textarea id='json' style='border: 1px solid #000; font-family: Consolas; height: 200px; width: 500px;'>${e.responseText}</textarea>`)
+    }
+  })
 
   // lazy load sites
   $('section iframe').recliner({
@@ -151,4 +150,4 @@
       $current.height(article_height * height_mod)
     }
   })
-}($));
+}(jQuery));
